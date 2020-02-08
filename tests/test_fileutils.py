63b7975bcb2225dd35a8813ca04359f8f9692ae6
ccdale <chris.allison@hivehome.com>
2020-02-08 21:03:52 +0000

    linter wants sentance endings

--- tests/test_fileutils.py
+++ tests/test_fileutils.py
@@ -1,4 +1,4 @@
-"""test file for fileutils.py file of ccautils module"""
+"""test file for fileutils.py file of ccautils module."""
 import os
 
 import ccautils.fileutils as FT
@@ -10,33 +10,33 @@ testlines = ["one\n", "two\n", "Three"]
 
 
 def test_fileExists():
-    """fileExists file exists test"""
+    """fileExists file exists test."""
     got = FT.fileExists(__file__)
     assert got is True
 
 
 def test_fileExists_nonexist():
-    """fileExists file doesn't exist test"""
+    """fileExists file doesn't exist test."""
     got = FT.fileExists("/non-existant")
     assert got is False
 
 
 def test_dirExists():
-    """dirExists directory exists test"""
+    """dirExists directory exists test."""
     dird = os.path.dirname(__file__)
     got = FT.dirExists(dird)
     assert got is True
 
 
 def test_dfExists():
-    """dfExists directory exists test"""
+    """dfExists directory exists test."""
     dird = os.path.dirname(__file__)
     got = FT.dfExists(dird)
     assert got is True
 
 
 def test_absPath():
-    """absPath file transform test"""
+    """absPath file transform test."""
     tpath = "~/.bashrc"
     epath = os.environ.get("HOME") + "/.bashrc"
     got = FT.absPath(tpath)
@@ -44,25 +44,25 @@ def test_absPath():
 
 
 def test_fileTouch():
-    """fileTouch test"""
+    """fileTouch test."""
     FT.fileTouch(testfile)
     assert FT.fileExists(testfile) is True
 
 
 def test_rename():
-    """rename file test"""
+    """rename file test."""
     FT.rename(testfile, renamefile)
     assert FT.fileExists(renamefile) is True
 
 
 def test_fileDelete():
-    """fileDelete test"""
+    """fileDelete test."""
     FT.fileDelete(renamefile)
     assert FT.fileExists(renamefile) is False
 
 
 def test_fileSize():
-    """fileSize test"""
+    """fileSize test."""
     with open(testfile, "w") as ofn:
         ofn.writelines(testlines)
     sz = FT.fileSize(testfile)
@@ -70,13 +70,13 @@ def test_fileSize():
 
 
 def test_sizeof_fmt():
-    """sizeof_fmt test"""
+    """sizeof_fmt test."""
     got = FT.sizeof_fmt(6445440)
     assert got == "6.1MB"
 
 
 def test_getFileHash():
-    """getFileHash test"""
+    """getFileHash test."""
     fhash, fsize = FT.getFileHash(testfile)
     assert (
         fhash == "181d226b4a56b29a8ace235df83c9b0758a9decb4d6a45d0a567a6ddd1e1c970"
@@ -85,7 +85,7 @@ def test_getFileHash():
 
 
 def test_readFile():
-    """readFile test"""
+    """readFile test."""
     xstr = ""
     for line in testlines:
         xstr += line
@@ -94,7 +94,7 @@ def test_readFile():
 
 
 def test_cleanup():
-    """cleanup after testing"""
+    """cleanup after testing."""
     FT.fileDelete(testfile)
     FT.fileDelete(renamefile)
     assert FT.fileExists(testfile) is False and FT.fileExists(renamefile) is False
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,9 +1,9 @@
-"""test file for utils.py file of ccautils module"""
+"""test file for utils.py file of ccautils module."""
 import ccautils.utils as UT
 
 
 def test_addToStringStr():
-    """addToString test string input"""
+    """addToString test string input."""
     xstr = "A String"
     astr = " added"
     zstr = UT.addToString(xstr, astr)
@@ -11,7 +11,7 @@ def test_addToStringStr():
 
 
 def test_addToStringList():
-    """addToString test list input"""
+    """addToString test list input."""
     xstr = "A String"
     lstr = [" ", "added"]
     zstr = UT.addToString(xstr, lstr)
@@ -19,7 +19,7 @@ def test_addToStringList():
 
 
 def test_delimitStringList():
-    """delimitString test list input"""
+    """delimitString test list input."""
     delim = " "
     xadd = ["A", "String", "added"]
     zstr = UT.delimitString(xadd, delim)
@@ -27,7 +27,7 @@ def test_delimitStringList():
 
 
 def test_makeDictFromString():
-    """makeDictFromString test"""
+    """makeDictFromString test."""
     istr = "someparam   = somevalue,someotherparam =someothervalue  "
     xd = UT.makeDictFromString(istr)
     wd = {
@@ -38,21 +38,21 @@ def test_makeDictFromString():
 
 
 def test_padStr_left():
-    """padStr on the left test"""
+    """padStr on the left test."""
     xstr = "3"
     ystr = UT.padStr(xstr)
     assert ystr == " 3"
 
 
 def test_padStr_right_zeros():
-    """padStr on the right with zeros test"""
+    """padStr on the right with zeros test."""
     xstr = "3"
     ystr = UT.padStr(xstr, 4, "0", False)
     assert ystr == "3000"
 
 
 def test_reduceTime():
-    """reduceTime test"""
+    """reduceTime test."""
     unit = 60
     secs = 100
     exp = (1, 40)
@@ -61,7 +61,7 @@ def test_reduceTime():
 
 
 def test_displayValue():
-    """displayValue multi-values test"""
+    """displayValue multi-values test."""
     val = 2
     label = "elephant"
     got = UT.displayValue(val, label)
@@ -69,7 +69,7 @@ def test_displayValue():
 
 
 def test_displayValue_one():
-    """displayValue single value test"""
+    """displayValue single value test."""
     val = 1
     label = "elephant"
     got = UT.displayValue(val, label)
@@ -77,7 +77,7 @@ def test_displayValue_one():
 
 
 def test_displayValue_zero():
-    """displayValue zero value test"""
+    """displayValue zero value test."""
     val = 0
     label = "elephant"
     got = UT.displayValue(val, label)
@@ -85,7 +85,7 @@ def test_displayValue_zero():
 
 
 def test_secondsFromHMS():
-    """secondsFromHMS test - no carry"""
+    """secondsFromHMS test - no carry."""
     hms = "01:01:23.43"
     secs = UT.secondsFromHMS(hms)
     exp = 3600 + 60 + 23
@@ -93,7 +93,7 @@ def test_secondsFromHMS():
 
 
 def test_secondsFromHMS_carry():
-    """secondsFromHMS test with carry"""
+    """secondsFromHMS test with carry."""
     hms = "01:01:23.73"
     secs = UT.secondsFromHMS(hms)
     exp = 3600 + 60 + 23 + 1
@@ -101,7 +101,7 @@ def test_secondsFromHMS_carry():
 
 
 def test_hms_small_short():
-    """hms test small short"""
+    """hms test small short."""
     secs = 67
     got = UT.hms(secs)
     exp = "1 min and 7 secs"
@@ -109,7 +109,7 @@ def test_hms_small_short():
 
 
 def test_hms_small():
-    """hms test small"""
+    """hms test small."""
     secs = 67
     got = UT.hms(secs, short=False)
     exp = "1 minute and 7 seconds"
@@ -117,7 +117,7 @@ def test_hms_small():
 
 
 def test_hms_long():
-    """hms test long"""
+    """hms test long."""
     secs = 67
     got = UT.hms(secs, small=False, short=False)
     exp = "0 days, 0 hours, 1 minute and 7 seconds"
@@ -125,7 +125,7 @@ def test_hms_long():
 
 
 def test_hms_single():
-    """hms test single"""
+    """hms test single."""
     secs = 86400 + 7200 + 300 + 34
     got = UT.hms(secs, single=True)
     exp = "1d 2h 5m 34s"
@@ -133,7 +133,7 @@ def test_hms_single():
 
 
 def test_hms_colons_full():
-    """hms test colons full"""
+    """hms test colons full."""
     secs = 86400 + 7200 + 300 + 34
     got = UT.hms(secs, colons=True)
     exp = "01:02:05:34"
@@ -141,7 +141,7 @@ def test_hms_colons_full():
 
 
 def test_hms_colons_short():
-    """hms test short colons"""
+    """hms test short colons."""
     secs = 302
     got = UT.hms(secs, colons=True)
     exp = "05:02"
@@ -149,7 +149,7 @@ def test_hms_colons_short():
 
 
 # def test_askMe():
-#     """askMe string input test"""
+#     """askMe string input test."""
 #     query = "Please press the"
 #     default = "enter key"
 #     v = UT.askMe(query, default)
@@ -157,7 +157,7 @@ def test_hms_colons_short():
 #
 #
 # def test_askMeNumeric():
-#     """askMe numeric input test"""
+#     """askMe numeric input test."""
 #     query = "press 5 and enter"
 #     default = "5"
 #     v = UT.askMe(query, default)
